7776f8875944f33513bb107450f742cde31512a2
이상철 <zlrz6706@gmail.com>
2023-04-20 06:26:36 +0900

    basic

---
+++ basic/07.조건문.js
@@ -0,0 +1,23 @@
+// if 문 switch문
+
+let age = 10;
+
+if (age === 10) {
+  console.log("나이는 10살입니다.");
+} else {
+  console.log("모르겠습니다.");
+}
+
+switch (age) {
+  case 1:
+    console.log(1);
+  case 2:
+    console.log(2);
+  case 3:
+    console.log(3);
+  case 4:
+    console.log(4);
+  default:
+    console.log("??");
+    break;
+}
